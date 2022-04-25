dc6686dea02627a771ad0c11e7a2ffd35de85971
Doğukan Kurnaz <dogukanertunga@gmail.com>
2022-04-25 05:03:46 +0300

    refactor

--- src/main/script.js
+++ src/main/script.js
@@ -1,9 +1,4 @@
-/**
- * Create google maps Map instance.
- * @param {number} lat
- * @param {number} lng
- * @return {Object}
- */
+
 const createMap = ({ lat, lng }) => {
   return new google.maps.Map(document.getElementById('map'), {
     center: { lat, lng },
@@ -11,23 +6,10 @@ const createMap = ({ lat, lng }) => {
   });
 };
 
-/**
- * Create google maps Marker instance.
- * @param {Object} map
- * @param {Object} position
- * @return {Object}
- */
 const createMarker = ({ map, position }) => {
   return new google.maps.Marker({ map, position });
 };
 
-
-/**
- * Track the user location.
- * @param {Object} onSuccess
- * @param {Object} [onError]
- * @return {number}
- */
 const trackLocation = ({ onSuccess, onError = () => { } }) => {
   if ('geolocation' in navigator === false) {
     return onError(new Error('Geolocation is not supported by your browser.'));
@@ -40,11 +22,6 @@ const trackLocation = ({ onSuccess, onError = () => { } }) => {
   });
 };
 
-/**
- * Get position error message from the given error code.
- * @param {number} code
- * @return {String}
- */
 const getPositionErrorMessage = code => {
   switch (code) {
     case 1:
